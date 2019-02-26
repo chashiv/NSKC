f8c084e081ab0a849df833ffbc6cd0793e82f879
Shivam Chauhan <chauhanshivam999@gmail.com>
2019-02-27 01:50:45 +0530

    Problem - 2 Implemented Infix to Postfix

---
+++ C4/infix_to_postfix.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std ;
+int getPriority(char ch )
+{
+	switch(ch)
+	{
+		case '+' :	
+		case '-' :	return 1;
+					break;
+		case '*' :	
+		case '/' :	return 2;
+					break;
+
+		case '^' :	return 3;
+					break;
+
+		default  :  return 0 ;
+					break ;
+
+	}
+}
+string infix_to_postfix(string str )
+{
+	stack<char>s;
+	string result = "";
+	for ( auto c : str)
+	{
+		if( ( c >= 'a' && c <= 'z' ) || (c >= 'A' && c <= 'Z') )
+		{
+			result += c ;
+		}
+		else if ( c == ')')
+		{
+			while ( !s.empty() && s.top() != '(' )
+			{
+				result += s.top();
+				s.pop();
+			}
+			if( !s.empty() && s.top() == '(' ) s.pop() ;
+		}
+		else
+		{
+			while( !s.empty() && getPriority(s.top()) >= getPriority(c) && c != '(')
+			{
+				result += s.top();
+				s.pop();
+			}
+			s.push(c);
+		}
+	}
+	while(!s.empty())
+	{
+		result += s.top(); 
+		s.pop();
+	}
+	return result ;
+}
+
+int main()
+{
+	int t ;
+	cin >> t ;
+	while(t--)
+	{
+		string str ;
+		cin >> str ;
+		cout << infix_to_postfix(str) << endl ;
+	}
+	return 0 ;
+}
