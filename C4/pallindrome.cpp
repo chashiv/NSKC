59dcdedc8a67092104758b6cb0aa10b7f132261c
Shivam Chauhan <chauhanshivam999@gmail.com>
2019-02-27 18:50:43 +0530

    Problem 10 - Check whether the given string is Pallindrome

---
+++ C4/pallindrome.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string isPallindrome(string str)
+{
+	stack<char>stck;
+	int i = 0 ;
+	while( str[i] != 'X' )
+	{
+		stck.push(str[i]); 
+		i++ ;
+	}
+	i++;
+	while(str[i])
+	{
+		if( stck.empty() || str[i] != stck.top() )
+		{
+			return "Not Pallindrome";
+		}
+		else
+		{
+			stck.pop();
+		}
+		i++;
+	}			
+	if ( stck.empty() )
+		return "Pallindrome" ;
+	else
+		return "Not Pallindrome" ;
+}
+int main()
+{
+	int t;
+	cin >> t ;
+	while(t--)
+	{
+		string str ;
+		cin >> str;
+		cout << isPallindrome(str) << endl ;
+	}
+	return 0 ;
+}
